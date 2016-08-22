a7c2287b48b36782b68d171ac46016ef84a5b468
jzq <495182828@qq.com>
2016-08-22 16:19:24 +0800

    first
     前端多页架构

---
+++ MPA/dev/js/index.js
@@ -0,0 +1,25 @@
+
+'use strict';
+require('../js/module/a.js'); 
+
+
+require('../js/libs/jquery.backgroundvideo.min'); //视频背景
+require('../js/libs/smoothscroll'); //滚动条平滑
+require('../css/public.less');
+require('../css/index.less');
+
+$(function(){
+    //视频背景
+   /* const videobackground = new $.backgroundVideo($('body'), {
+      "align": "centerXY",
+      "width": 1280,
+      "height": 720,
+      "path": "video/",
+      "filename": "video",
+      "types": ["mp4","webm"]
+    });*/
+
+
+
+})
+
---
+++ MPA/dev/js/module/a.js
@@ -0,0 +1,2 @@
+require('../../js/libs/jquery.min-1.9.1.js'); 
+require('../../js/libs/bootstrap.min.js'); 
