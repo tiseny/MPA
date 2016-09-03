75d3d7750a2face697b8da8fafd30f35f32a03b2
jzq <495182828@qq.com>
2016-09-03 17:50:32 +0800

    去掉proxy

--- config.js
+++ config.js
@@ -2,7 +2,6 @@ module.exports = {
   serverHostName: 'localhost',
   serverPort: 8080,
   publicPath: 'http://localhost:8080/',
-  proxyPath: 'http://localhost:54999',
   devPath: './src/',
   distPath: './dist/'
 }
