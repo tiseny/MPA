59ea6eb2097eccbb917396c3c8fe99b6e34103f4
jiangzq <jiangzq@mysoft.com.cn>
2016-09-07 18:46:45 +0800

    增加 article 模块 插件

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,29 @@
 import '../css/index.less';
-console.log($('body'));
-console.log('index.js');
-
 import data from '../mock/header';
+import article from '../modules/article';
+
 const template = require('../libs/art-template');
 
 $(function() {
-  $.ajax({
-    url: 'http://g.cn',
-    dataType:'json'
-  }).done((data, status, xhr) => {
-    var html = template('tpl', data);
-    $('#row').html(html);   
-  })  
+  // $.ajax({
+  //   url: 'http://g.cn',
+  //   dataType:'json'
+  // }).done((data, status, xhr) => {
+  //   var html = template('tpl', data);
+  //   $('#row').html(html);   
+  // }) 
+  // 生成
+  article.init({
+    container: $('#article'),
+    data:{
+      title:'你好呀，李银河！',
+      sections: [{
+          text: '段落文字段落文字段落文字段落文字段落文字段落文字段落文字段落文字段落文字段落文字段落文字段落文字段落文字段落文字段落文字段落文字'
+        },{
+          text: '段落文字段落文字段落文字段落文字段落文字段落文字段落文字段落文字段落文字段落文字段落文字段落文字段落文字段落文字段落文字段落文字段落文字段落文字段落文字段落文字'
+        }
+      ]
+    }
+  })
+
 })
---
+++ src/modules/article.js
@@ -0,0 +1,70 @@
+/**
+ * article 
+ * @type {Object}
+ */
+import './article.less';
+
+const article = {
+  defaultProps: {
+    container: $('body'),
+    data: {
+      title: '标题',
+      sections: [{
+          text: '段落文字',
+          style: {
+            color: 'red'
+          }
+        },{
+          text: '段落文字',
+          style: {
+            color: 'green'
+          }
+        }
+      ]
+    }  
+  },
+  // 初始化的时候，需要确认配置是否正确
+  init: (options) => {
+    article.createStructs(options ? options : article.defaultProps)
+  },
+  // settings 组织结构
+  createStructs: (opts) => {
+    opts.container.append("<article>" + article.inject(opts.data) + "</article>")
+
+    // 事件绑定
+    article.onEvents(opts)
+
+  },
+  // 注入相关内容
+  inject: (data = data || article.defaultProps.data) => {
+    let close_btn = "<span class='close'></span>";
+    let title = "<div class='title brage'>" + data.title + "</div>";
+    let sections = ""
+    Array.isArray(data.sections) && data.sections.forEach(section => {
+      // json 字符串去掉 ｛｝
+      let style = section.style ? JSON.stringify(section.style) : ''
+      style = style.length > 0 ? style.substr(1,style.length - 2).replace(new RegExp('"',"gm"),'') : ''
+      
+      sections += "<section style=" + style + ">" + section.text + "</section>"
+    })
+
+    return close_btn + title + sections
+  },
+  // 关闭按钮
+  onEvents: (opts) => {
+    ['remove','dblclick'].forEach(item => {
+      switch(item) {
+        case 'remove': return opts.container.on('click', 'article .close', function(e){
+          e.stopPropagation();
+          opts.container.remove();
+        });
+        case 'dblclick': return opts.container.on(item, function(){
+          console.log(212)
+        });
+      }
+    })
+  }
+
+}
+
+export default article;
